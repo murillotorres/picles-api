77b7b5af25ff6b478a5c765b1c87b461963d585e
murillotttorres@live.com <murillotttorres@live.com>
2024-04-03 22:09:03 -0300

    Adicionando validação no shelter input

--- src/shelter/dtos/update.shelter.controller.input.ts
+++ src/shelter/dtos/update.shelter.controller.input.ts
@@ -1,7 +1,23 @@
+import { IsNumber, IsNumberString, IsString, Length, IsEmail, IsNotEmpty} from "class-validator"
+
 // ATUALIZAÇÃO DE INFORMAÇÕES SOBRE O ABRIGO DE ANIMAIS
 export default class UpdateShelterControllerInput {
+    @IsString()
+    @IsNotEmpty()
     name: string
+    
+    @IsString()
+    @Length(10,11)
+    @IsNotEmpty()
     whatsapp: string
+    
+    @IsString()
+    @IsNumberString()
+    @IsNotEmpty()
     phone: string
+
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
     email: string
 }
